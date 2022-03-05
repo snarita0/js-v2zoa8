e5d927e044f106f2f0b97a82cfd0f678201f83af
snarita0 <stephan.narita@gmail.com>
2022-03-05 11:57:54 -0500

    1 file modified, 5 added, 26 deleted

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { Test } from './scripts/Test'
-import './styles/app.css'
+import { Test } from './src/scripts/Test'
+import './src/styles/app.css'
 
 const app = async () => {
   const root = document.getElementById("root");
--- dist/index.1a112141.js
+++
@@ -1,2 +0,0 @@
-(async()=>{document.getElementById("root").innerHTML+="\n    <header>\n      <h1>My Parcel App</h1>\n      <p>This is a boilerplate for a simple vanilla JS workflow using the Parcel bundler.</p>\n    </header>\n  "})();
-//# sourceMappingURL=index.1a112141.js.map
