2ee7a61c669137ccae04b80913ab4421f57ba7e3
stephann0 <stephan.narita@gmail.com>
2022-03-15 07:21:31 -0400

    Set 2 Challenge 3 Finished : 1 file modified

--- src/scripts/course-challenges/js-fundamentals-part2/set2_challenge3.js
+++ src/scripts/course-challenges/js-fundamentals-part2/set2_challenge3.js
@@ -10,6 +10,8 @@ Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 */
 
 export const set2_challenge3 = () => {
+  // calcBMI() is defined as is in each object
+  // to follow suit with the challenge
   let markBMIData = {
     fullName: 'Mark Miller',
     weight: 78,
@@ -18,7 +20,6 @@ export const set2_challenge3 = () => {
       return mass / height ** 2;
     },
   };
-  console.log(markBMIData.BMI);
   let johnBMIData = {
     fullName: 'John Smith',
     weight: 92,
@@ -28,33 +29,25 @@ export const set2_challenge3 = () => {
     },
   };
 
-  let { weight: markWeight, height: markHeight } = markBMIData;
-  let { weight: johnWeight, height: johnHeight } = johnBMIData;
+  let {
+    fullName: markFullName,
+    weight: markWeight,
+    height: markHeight,
+    calcBMI,
+  } = markBMIData;
+  let {
+    fullName: johnFullName,
+    weight: johnWeight,
+    height: johnHeight,
+  } = johnBMIData;
 
   markBMIData.BMI = calcBMI(markWeight, markHeight);
   johnBMIData.BMI = calcBMI(johnWeight, johnHeight);
 
-  let { BMI: markBMI } = markBMIData;
-  let { BMI: johnBMI } = johnBMIData;
-  let markHigherBMI = markBMI > johnBMI;
+  let markHigherBMI = markBMIData.BMI > johnBMIData.BMI;
 
-  //prettier-ignore
-  console.log(`Set 1: ${markHigherBMI_set1} (${markBMI_set1.toFixed(3)} > 
-  ${johnBMI_set1.toFixed(3)})
-  Set 2: ${markHigherBMI_set2} (${markBMI_set2.toFixed(3)} < 
-  ${johnBMI_set2.toFixed(3)})`);
-
-  //prettier-ignore
-  if (markHigherBMI_set1) {
-    console.log(`Mark's BMI (${markBMI_set1.toFixed(3)}) is higher than John's (${johnBMI_set1.toFixed(3)})!`);
-  } else {
-    console.log(`Mark's BMI ( ${markBMI_set1.toFixed(3)}) is lower than John's (${johnBMI_set1.toFixed(3)})!`);
-  }
-
-  //prettier-ignore
-  if (markHigherBMI_set2) {
-    console.log(`Mark's BMI ( ${markBMI_set2.toFixed(3)}) is higher than John's (${johnBMI_set2.toFixed(3)})!`);
-  } else {
-    console.log(`Mark's BMI ( ${markBMI_set2.toFixed(3)}) is lower than John's (${johnBMI_set2.toFixed(3)})!`);
-  }
+  // prettier-ignore
+  markHigherBMI ? 
+  console.log(`${markFullName}'s BMI (${markBMIData.BMI.toFixed(3)}) is higher than ${johnFullName}'s (${johnBMIData.BMI.toFixed(3)})!`) :
+  console.log(`${markFullName}'s BMI (${markBMIData.BMI.toFixed(3)}) is lower than ${johnFullName}'s (${johnBMIData.BMI.toFixed(3)})!`);
 };
