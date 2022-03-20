7fb0ae1687d3486b2d45e6da8a872f5ed8630d7b
stephann0 <stephan.narita@gmail.com>
2022-03-20 07:31:06 -0400

    Save Files : 1 file modified

--- src/pages/course-challenges/dom-and-events/script.js
+++ src/pages/course-challenges/dom-and-events/script.js
@@ -8,10 +8,6 @@ Your tasks:
   4. Also restore the original background color (#222) and number width (15rem)
 */
 
-// Generate random number from 1 - 20
-// Grab number input
-// Test if match
-
 // #60b347
 
 const generateRandomInt = (min = 1, max = 20) => {
@@ -19,32 +15,63 @@ const generateRandomInt = (min = 1, max = 20) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-let timeToLoadContent = 1000;
-let hiddenNumber = generateRandomInt();
+let timeToLoadContent = 500;
 
 // Give DOM enough time to load, to access it.
 setTimeout(() => {
   const againButton = document.querySelector('.again');
   const message = document.querySelector('.message');
-  const score = document.querySelector('.score');
-  const secretNumber = document.querySelector('.number');
-
   const userGuess = document.querySelector('.guess');
+  const score = document.querySelector('.score');
+  const highscore = document.querySelector('.highscore');
+  const secretNumber = document.querySelector('.number');
   const checkButton = document.querySelector('.check');
-
   const documentBody = document.getElementsByTagName('body')[0];
 
+  let hiddenNumber = generateRandomInt();
+  const newGame = () => {
+    documentBody.style.backgroundColor = '#222';
+    score.innerText = '20';
+    secretNumber.innerText = '?';
+    message.innerText = 'Start guessing...';
+    userGuess.value = null;
+    checkButton.disabled = false;
+    hiddenNumber = generateRandomInt();
+  };
+
+  function keyListener(event) {}
+
+  checkButton.addEventListener('keydown', (event) => {
+    const ENTER_KEY = 13;
+    event = event || window.event;
+
+    if (event.key === ENTER_KEY) {
+      // Create a newGame function to wrap the click event code around
+      // If Enter key is pressed, run newGame()
+      console.log('Enter pressed');
+    }
+  });
+
   checkButton.addEventListener('click', () => {
-    if (userGuess.value === hiddenNumber) {
-      // change background color
-      // reeval secretNumber with the hidden number.
+    let gameWon = parseInt(userGuess.value) === hiddenNumber;
+    let newHighscore =
+      parseInt(score.innerText) > parseInt(highscore.innerText);
+
+    if (gameWon) {
+      documentBody.style.backgroundColor = '#60b347';
+      secretNumber.innerText = hiddenNumber;
+      message.innerText = 'You Win!';
+      checkButton.disabled = true;
+
+      if (newHighscore) {
+        highscore.innerText = score.innerText;
+      }
+    } else {
+      score.innerText = parseInt(score.innerText) - 1;
     }
   });
 
   againButton.addEventListener('click', () => {
-    score.innerText = 0;
-    secretNumber.innerText = '?';
-    message.innerText = 'Start guessing...';
-    userGuess.value = null;
+    newGame();
   });
 }, timeToLoadContent);
