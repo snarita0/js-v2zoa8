6bcbea1887f7306e779af92fae52e13ddba105b9
stephann0 <stephan.narita@gmail.com>
2022-03-22 05:07:05 -0400

    Save Files : 1 file modified

--- src/pages/course-challenges/dom-and-events/script.js
+++ src/pages/course-challenges/dom-and-events/script.js
@@ -6,9 +6,11 @@ Your tasks:
   2. In the handler function, restore initial values of the 'score' and 'secretNumber' variables
   3. Restore the initial conditions of the message, number, score and guess input fields
   4. Also restore the original background color (#222) and number width (15rem)
-*/
 
-// #60b347
+  When listening for "keydown" event, the keys  being listened to get spammed multiple times. The number of times spammed grows overtime while playing the game. If played long enough, pressing those keys will cause a seizure.
+
+
+*/
 
 const generateRandomInt = (min = 1, max = 20) => {
   // Includes min and max
@@ -18,7 +20,9 @@ const generateRandomInt = (min = 1, max = 20) => {
 let timeToLoadContent = 500;
 
 // Give DOM enough time to load, to access it.
-setTimeout(() => {
+// setTimeout(() => {}, timeToLoadContent);
+
+document.addEventListener('DOMContentLoaded', () => {
   const againButton = document.querySelector('.again');
   const message = document.querySelector('.message');
   const userGuess = document.querySelector('.guess');
@@ -33,26 +37,15 @@ setTimeout(() => {
     documentBody.style.backgroundColor = '#222';
     score.innerText = '20';
     secretNumber.innerText = '?';
+    secretNumber.style.width = 'calc(15rem / var(--document-downscale))';
     message.innerText = 'Start guessing...';
     userGuess.value = null;
     checkButton.disabled = false;
-    hiddenNumber = generateRandomInt();
+    hiddenNumber = 1;
+    // hiddenNumber = generateRandomInt();
   };
 
-  function keyListener(event) {}
-
-  checkButton.addEventListener('keydown', (event) => {
-    const ENTER_KEY = 13;
-    event = event || window.event;
-
-    if (event.key === ENTER_KEY) {
-      // Create a newGame function to wrap the click event code around
-      // If Enter key is pressed, run newGame()
-      console.log('Enter pressed');
-    }
-  });
-
-  checkButton.addEventListener('click', () => {
+  const gameWonCheck = () => {
     let gameWon = parseInt(userGuess.value) === hiddenNumber;
     let newHighscore =
       parseInt(score.innerText) > parseInt(highscore.innerText);
@@ -60,6 +53,7 @@ setTimeout(() => {
     if (gameWon) {
       documentBody.style.backgroundColor = '#60b347';
       secretNumber.innerText = hiddenNumber;
+      secretNumber.style.width = '17.5rem';
       message.innerText = 'You Win!';
       checkButton.disabled = true;
 
@@ -69,9 +63,24 @@ setTimeout(() => {
     } else {
       score.innerText = parseInt(score.innerText) - 1;
     }
+  };
+
+  document.addEventListener('keydown', (event) => {
+    const ENTER_KEY = 'Enter';
+    const QUOTE_KEY = 'Quote';
+
+    if (event.code === ENTER_KEY) {
+      // gameWonCheck();
+      console.log('Enter pressed');
+    }
+    if (event.code === QUOTE_KEY) {
+      // newGame();
+      console.log('Apostraphe pressed');
+    }
+
+    // event.preventDefault();
   });
 
-  againButton.addEventListener('click', () => {
-    newGame();
-  });
-}, timeToLoadContent);
+  checkButton.addEventListener('click', gameWonCheck());
+  againButton.addEventListener('click', newGame());
+});
